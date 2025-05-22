ee81fd22fcf3a34913f0fa1b14a873e2ea84a90a
DANI <111095710+thayssaromao@users.noreply.github.com>
2025-05-21 22:17:07 -0300

    main

---
+++ main.py
@@ -0,0 +1,15 @@
+#Projeto Dashboard Sistemas Operacionais
+import time
+
+#Importar funçôes para dash (cpu, memoria, disco)
+
+def main():
+  print("passou")
+  while True:
+ #dash roda para 'sempre' e atualiza a cada 5 segundos
+
+ #Estrututa visual provisória do Dash
+    print("="*30)
+    print("DASHBOARD DO SISTEMA")
+    print("="*30)
+
