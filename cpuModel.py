b954ce36dc3d7babc953a932aae736d1fe57d92d
DANI <111095710+thayssaromao@users.noreply.github.com>
2025-05-22 03:00:56 +0000

    Uso da Cpu + Aplicacao na Main

--- cpuModel.py
+++ cpuModel.py
@@ -2,9 +2,38 @@
     Ler dados sobre o uso da CPU
     retornar valor em porcentagem
     """
-    
+import time
+
 def lerUsoCpu():
     
+    #
+    #proc arquivos de interface do kernel
+    #/proc/stat arquivo com estatísticas sobre o uso da CPU
+    # usoCPU = 100% - tempo_ocioso / tempo_total
+
     with open("/proc/stat", "r") as f:
-        linha = f.readline() 
+        linha = f.readline() #le a primeira linha
+        partes1 = linha.split()[1:]
+        valores1 = list(map(int, partes1))
+        
+    time.sleep(5)
+
+    with open("/proc/stat", "r") as f:
+        linha = f.readline()
+        partes2 = linha.split()[1:]
+        valores2 = list(map(int, partes2))
+
+    tempo1 = sum(valores1) #soma de todos os tempos registrados
+    tempo2 = sum(valores2)
+
+    #idle -> tempo de ociosidade da CPU
+    idle1 = valores1[3]
+    idle2 = valores2[3]
+
+    tempoDiff = tempo2 - tempo1
+    idleDiff = idle2 - idle1
+    ociosidade = tempoDiff - idleDiff
+    usoCpu = 100 * (ociosidade / tempoDiff)
+
+    return round(usoCpu, 2)
    
--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 #Projeto Dashboard Sistemas Operacionais
-import time
 
 #Importar funçôes para dash (cpu, memoria, disco)
+import time
+from cpuModel import lerUsoCpu
 
-def main():
-  print("passou")
-  while True:
- #dash roda para 'sempre' e atualiza a cada 5 segundos
-
- #Estrututa visual provisória do Dash
-    print("="*30)
-    print("DASHBOARD DO SISTEMA")
-    print("="*30)
+uso = lerUsoCpu()
+print(f"Uso da CPU: {uso}%")
 
