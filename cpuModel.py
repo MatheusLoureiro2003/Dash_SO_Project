86eda93dc6306f80efd9aa11a5f5118ac63c6edb
DANI <111095710+thayssaromao@users.noreply.github.com>
2025-05-24 18:21:51 -0300

    Update cpuModel.py

--- cpuModel.py
+++ cpuModel.py
@@ -1,23 +1,25 @@
-"""
-    Ler dados sobre o uso da CPU
-    retornar valor em porcentagem
-    """
-
+import threading
 import time
 
+lock = threading.Lock() #trava que controla o acesso seguro a recursos compartilhados em threads.
+uso_cpu_global = None
+
+
 def lerUsoCpu():
-    
-    #
-    #proc arquivos de interface do kernel
-    #/proc/stat arquivo com estatísticas sobre o uso da CPU
-    # usoCPU = 100% - tempo_ocioso / tempo_total
+
+    '''
+     'proc' arquivos de interface do kernel
+     /proc/stat arquivo com estatísticas sobre o uso da CPU
+     usoCPU = 100% - tempo_ocioso / tempo_total
+
+    '''
 
     with open("/proc/stat", "r") as f:
         linha = f.readline() 
         partes1 = linha.split()[1:]
         valores1 = list(map(int, partes1))
         
-    time.sleep(5)
+    time.sleep(5) #bloqueante
 
     with open("/proc/stat", "r") as f:
         linha = f.readline()
@@ -35,35 +37,31 @@ def lerUsoCpu():
 
     tempoAtivo = tempoDiff - idleDiff #tempo em que a CPU realmente trabalhou
 
-    usoCpu = 100 * (tempoAtivo / tempoDiff)
+    usoCpu = 100 * (tempoAtivo / tempoDiff) #Uso da CPU em porcentagem
 
-    return round(usoCpu, 2)
+    ociosidade = 100 * (idleDiff / tempoDiff) 
 
+    return round(usoCpu, 2), round(ociosidade, 2)
 
-def lerOciosidadeCpu():
-    with open("/proc/stat", "r") as f:
-        linha = f.readline()
-        partes1 = linha.split()[1:]
-        valores1 = list(map(int, partes1))
+def monitorarCpu():
+    global uso_cpu_global
 
-    time.sleep(5)
+    # Chama lerUsoCpu que já inclui a pausa de 5 segundos internamente
+    uso, ocioso = lerUsoCpu()
 
-    with open("/proc/stat", "r") as f:
-        linha = f.readline()
-        partes2 = linha.split()[1:]
-        valores2 = list(map(int, partes2))
+    # Protege a atualização da variável global com lock
+    with lock:
+        uso_cpu_global = uso
+        ociosidade = ocioso
 
-    tempo1 = sum(valores1)
-    tempo2 = sum(valores2)
+    print(f"Uso CPU atualizado: {uso_cpu_global}%")
+    print(f"Ociosidade CPU: {ociosidade}%")
+        #Dentro do with lock, a variável global uso_cpu_global é atualizada com segurança, 
+        # para que outras threads não leiam ela enquanto está sendo modificada.
 
-    tempoOcioso1 = valores1[3]
-    tempoOcioso2 = valores2[3]
+def thread_monitor():
+    while True:
+        monitorarCpu()
 
-    tempoDiff = tempo2 - tempo1
-    idleDiff = tempoOcioso2 - tempoOcioso1
-
-    ociosidade = 100 * (idleDiff / tempoDiff)
-
-    return round(ociosidade, 2)
-
-   
+t = threading.Thread(target=thread_monitor)
+t.start()
