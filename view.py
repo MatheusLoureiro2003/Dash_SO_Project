c93b10fa9736a99466e2236ebebada2d06822edb
DANI <111095710+thayssaromao@users.noreply.github.com>
2025-07-03 02:26:00 -0300

    Update view.py

--- view.py
+++ view.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from customtkinter import CTkLabel, CTkTextbox, CTkFrame
-
+#versao thayssa
 # --- Widgets globais ----------------------------------------------------------
 uso_cpu_label = None
 ociosidade_label = None
